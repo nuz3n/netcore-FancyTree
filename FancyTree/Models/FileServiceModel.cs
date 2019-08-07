309782471bd16f49825313ad91de5b26d436ec37
nuz3n <niti.lembaroeem@gmail.com>
2019-08-07 18:22:25 +0700

    first upload project

---
+++ FancyTree/Models/FileServiceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FancyTree.Models
+{
+    public class FileServiceModel
+    {
+        public string key { get; set; }
+        public string title { get; set; }
+        public bool folder { get; set; }
+        public bool lazy { get; set; }
+
+        public FileServiceModel()
+        {
+            folder = false;
+            lazy = false;
+        }
+    }
+}
---
+++ FancyTree/WebApi/FileServiceController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FancyTree.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FancyTree.WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileServiceController : ControllerBase
+    {
+        private static string[] DEFUALT_FOLDER = { "logs", "UploadFiles" };
+        private static string[] DEFUALT_MIME_TYPE = { };
+        private readonly IHostingEnvironment _env;
+
+        public FileServiceController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        [Route("HeadList")]
+        public JsonResult HeadList()
+        {
+            List<FileServiceModel> fileModel = new List<FileServiceModel>();
+
+            var contentRoot = _env.ContentRootPath;
+
+            var dirnames = from fullDirname in Directory.EnumerateDirectories(contentRoot)
+                            select fullDirname.Substring(fullDirname.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+
+            foreach (var dir in dirnames.Where(a => DEFUALT_FOLDER.Contains(a)))
+            {
+                FileServiceModel model = new FileServiceModel();
+                model.key = dir;
+                model.title = dir;
+                model.lazy = true;
+                model.folder = true;
+                fileModel.Add(model);
+            }
+
+            //var filenames = from fullFilename in Directory.EnumerateFiles(contentRoot, "*")
+            //                select Path.GetFileName(fullFilename);
+
+            //foreach (string file in filenames)
+            //{
+            //    FileServiceModel model = new FileServiceModel();
+            //    model.key = file;
+            //    model.title = file;
+            //    model.lazy = false;
+            //    model.folder = false;
+            //    fileModel.Add(model);
+            //}
+
+            return new JsonResult(fileModel);
+        }
+
+        [HttpGet]
+        [Route("NodeList")]
+        public JsonResult NodeList(string path)
+        {
+            List<FileServiceModel> fileModel = new List<FileServiceModel>();
+
+            var contentRoot = _env.ContentRootPath;
+            var searchPath = Path.Combine(contentRoot, path);
+
+            var dirnames = from fullDirname in Directory.EnumerateDirectories(searchPath)
+                select fullDirname.Substring(fullDirname.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+
+            foreach (var dir in dirnames)
+            {
+                FileServiceModel model = new FileServiceModel();
+                model.key = path + "/" + dir;
+                model.title = dir;
+                model.lazy = true;
+                model.folder = true;
+                fileModel.Add(model);
+            }
+
+            var filenames = from fullFilename in Directory.EnumerateFiles(searchPath, "*")
+                select Path.GetFileName(fullFilename);
+
+            foreach (string file in filenames)
+            {
+                FileServiceModel model = new FileServiceModel();
+                model.key = path + "/" + file;
+                model.title = file;
+                model.lazy = false;
+                model.folder = false;
+                fileModel.Add(model);
+            }
+
+            return new JsonResult(fileModel);
+        }
+
+        [HttpGet]
+        [Route("DownloadFile")]
+        public ActionResult DownloadFile(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return null;
+            }
+
+            var contentRoot = _env.ContentRootPath;
+            var pathFile = Path.Combine(contentRoot, path);
+
+            if (!System.IO.File.Exists(pathFile))
+            {
+                return null;
+            }
+
+            var fileStream = new FileStream(pathFile, FileMode.Open, FileAccess.Read);
+            var result = new FileStreamResult(fileStream, "application/octet-stream");
+            result.FileDownloadName = Path.GetFileName(pathFile);
+            return result;
+        }
+    }
+}
